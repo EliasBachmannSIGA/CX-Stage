cbc146fcd40bbe989826f2d481d27e6dc397280d
EliasBachmannSIGA <Elias.Bachmann@siga.swiss>
2025-04-28 14:44:28 +0200

    PUT/PATCH

--- api/backend.php
+++ api/backend.php
@@ -4,7 +4,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $addresses = json_decode(file_get_contents('data.json'));
 
 switch($method) {
-    case 'GET':
+    case 'GET': 
         $data = $addresses;
         
         header("Content-Type: application/json");
@@ -50,15 +50,48 @@ switch($method) {
             exit;
         }
 
-    case 'PUT':
-    case 'PATCH':
+    case 'PUT':     
+    case 'PATCH':     
+        // Get the input data
+        parse_str(file_get_contents('php://input'), $_PUT_PATCH);
+
+        // Initialize a flag to check if the address already exists
+        $foundAddressKey = null;
         
-        break;
+        // Check if the prename and lastname already exist in the data
+        foreach ($addresses as $key => $address) {
+            if ($address->id == $_PUT_PATCH['id']) {
+                $foundAddressKey = $key;
+                break;
+            }
+        }
+
+        if ($foundAddressKey == null) {
+            http_response_code(400);
+            echo json_encode(["message" => "Address doesn't exist"]);
+            exit;
+        }
+
+        $changedAdress = [
+            'prename' => $_PUT_PATCH['prename'],
+            'lastname' => $_PUT_PATCH['lastname'],
+            'id' => $_PUT_PATCH['id']
+        ];
+
+        $addresses[$foundAddressKey] = $changedAdress;
+
+        // Save the updated addresses back to the data.json
+        file_put_contents('data.json', json_encode($addresses));
+
+        // Send the new address as the response
+        header("Content-Type: application/json");
+        echo json_encode($changedAdress);
+        exit;                
     case 'DELETE':
-        
+    
         break; 
     default:
         die('unknown http method requested');
-        
+
 }
  
